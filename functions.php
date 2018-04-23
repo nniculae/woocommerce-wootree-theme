fd8bad523477753cd9fcc9b302078610f6ada05e
Niculae Niculae <niculaenick@gmail.com>
2018-04-24 00:13:11 +0200

    Global wrapper added

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 add_theme_support( 'menus' );
 add_theme_support( 'post-thumbnails' );
 add_theme_support( 'title-tag' );
-add_theme_support( 'woocommerce' );
+add_theme_support( 'woocommerce' ); 
 
 function wpt_excerpt_length( $length ) {
 	return 16;
